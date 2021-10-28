9fba0e494c6e015d510333f55c39d015ebe5350c
etimarky <64045477+etimarky@users.noreply.github.com>
2021-10-27 22:19:17 -0400

    updates to cors

--- server/index.js
+++ server/index.js
@@ -4,15 +4,17 @@
 
 import { API } from '../main/axios.js';
 import express from 'express'
+import cors from 'cors';
 
 const axios = new API();
 // create new express app and save it as a constant
 const app = express();
+app.use(cors());
 // server configuration
 const PORT = 8080;
 
 //route created for the app with data
-app.get('/', (req, res) => {
+app.get('/', cors(), (req, res) => {
 
   return axios.searchDetails('netflix', 'series', '4', 'en').then(response => {
     res.send(JSON.stringify({ message: response }));
